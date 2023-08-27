d80f3b8466d09b72da25cbbb14e5f4248ca19e78
izzy <izder456@disroot.org>
2023-08-27 01:35:21 -0500

    fix title tagg (oh and crap i  whoopssss

--- src/srcerizder_site/web.clj
+++ src/srcerizder_site/web.clj
@@ -32,7 +32,7 @@
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport"
             :content "width=device-width, initial-scale=1.0"}]
-    [:title "Tech blog"]
+    [:title "Srcerizder!"]
     [:link {:rel "stylesheet" :href (link/file-path request "/styles/main.css")}]]
    [:body
     [:div.body page]]))
